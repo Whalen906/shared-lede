f167661eed92fa0e6c3d7af5dea7a147610c2e8e
Whalen906 <106979369+Whalen906@users.noreply.github.com>
2022-11-13 17:23:11 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -2,3 +2,5 @@
 
 #修改登录IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#复制内核5.10版本CPU超频补丁
+cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
